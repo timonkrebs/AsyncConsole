25e3e435637a5bb55ac7a417e48b1d5e1948ef3e
Timon Krebs <timon.krebs@zuehlke.com>
2021-09-16 14:06:47 +0200

    initial commit

--- AsyncConsole/AsyncPump.cs
+++ AsyncConsole/AsyncPump.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Microsoft.Threading
+namespace AsyncConsole
 {
     /// <summary>
     /// https://devblogs.microsoft.com/pfxteam/await-synchronizationcontext-and-console-apps/
